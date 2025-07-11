89c247ee9dd24faa0d0d7e583ae6e5f33bb97467
YahSeh <48594848+YahSeh@users.noreply.github.com>
2025-07-11 18:23:56 +0200

    Update bank.py

--- Problem Set 1/bank.py
+++ Problem Set 1/bank.py
@@ -1,11 +1,16 @@
 def main():
-    greet = input("Greeting : ").lower().replace(" ", "")
-    if "hello" in greet : # and greet[0] == "h" :
-        print("$0")
-    elif greet[0] == "h" and "hello" not in greet :
-        print("$20")
-    else :
-        print("$100")
-    return
+    greet = input("Bank's greeting: ")
+    due = greeting(greet)
+    print(f"${due}")
 
-main()
+def greeting(greet):
+    greet = greet.lower().strip()
+    if 'hello' in greet:
+        return 0
+    elif greet[0] == "h":
+        return 20
+    else:
+        return 100
+
+if __name__ == "__main__":
+    main()
